14a181a7f8d0e7c32558dc4fd1f178df5f236a49
Parvathareddy Nagavarun Kumar <119427732+Nagavarunkumarparvathareddy@users.noreply.github.com>
2024-09-27 20:59:14 +0530

    Delete MountBlue Job Challenge directory

--- MountBlue Job Challenge/2.Simple Array Sum.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-arr = list(map(int,input().split()))
-sum_arr = 0
-for ele in arr:
-    sum_arr += ele
-print(sum_arr)
--- MountBlue Job Challenge/5.Staricase.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-space = 0
-hash_symbol = 0
-for i in range(1,n+1):
-    space = (n-i)*" "
-    #print(space,type(space))
-    hash_symbol = i*'#'
-    #print(hash_symbol,type(hash_symbol))
-    print(space+hash_symbol)
